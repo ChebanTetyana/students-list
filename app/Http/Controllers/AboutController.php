622c5493232e0705d6bc213ac8ce7f3f8a69a148
ChebanTetyana <kapitoskay1302@gmail.com>
2024-04-10 21:35:48 +0300

    Define direction in tab about and home

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-class HomeController extends Controller
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
 {
     public function index()
     {
