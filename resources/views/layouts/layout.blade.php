986b186ddd60df0b4bcb751b082b0e607b6a6d8d
ChebanTetyana <kapitoskay1302@gmail.com>
2024-04-21 21:40:45 +0300

    Apply some styles for layout

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -8,7 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
           rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
           crossorigin="anonymous">
-{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
 </head>
 <style>
     .container-custom {
@@ -16,27 +15,41 @@
         margin-left: auto;
         margin-right: auto;
     }
+    .about {
+        background-image: url('images/about.jpg');
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        padding: 50px;
+        height: 100vh;
+    }
 </style>
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container-custom">
             <div class="d-flex justify-content-center">
                 <a class="navbar-brand" href="/"><img src="{{ asset('images/logo.png') }}"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+                        aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav gap-5 fs-4  mx-auto">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
-                        <a class="nav-link" href="/groups">Groups</a>
-                        <a class="nav-link" href="/students">Students</a>
-                        <a class="nav-link" href="/rating">Rating</a>
+                    <div class="navbar-nav gap-5 fs-4 mx-auto">
+                        <a class="nav-link {{ request()->is('/') ? 'active' : '' }}" href="/">Home</a>
+                        <a class="nav-link {{ request()->is('groups') ? 'active' : '' }}" href="/groups">Groups</a>
+                        <a class="nav-link {{ request()->is('students') ? 'active' : '' }}" href="/students">Students</a>
+                    </div>
                     </div>
                 </div>
             </div>
         </div>
     </nav>
     @yield('content')
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+            crossorigin="anonymous"></script>
+
 </body>
 </html>
 
