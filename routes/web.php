ed57204056a9f97af5dcfd1c06304b524e393a7b
ChebanTetyana <kapitoskay1302@gmail.com>
2024-04-21 21:41:40 +0300

    Delete route students show

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return redirect()->route('home.index');
-//});
-
 Route::get('/', [AboutController::class, 'index']);
-
 Route::get('/home', [AboutController::class, 'index'])->name('home.index');
 
 Route::resource('groups', 'GroupController');
@@ -42,7 +37,6 @@ Route::get('/students/sort-by-creation-date', [StudentController::class, 'sortBy
 Route::get('/students', [StudentController::class, 'index'])->name('students.index');
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
 Route::post('/students', [StudentController::class, 'store'])->name('students.store');
-Route::get('/students/{student}', [StudentController::class, 'show'])->name('students.show');
 Route::get('/students/{student}/edit', [StudentController::class, 'edit'])->name('students.edit');
 Route::put('/students/{student}', [StudentController::class, 'update'])->name('students.update');
 Route::delete('/students/{student}', [StudentController::class, 'destroy'])->name('students.destroy');
